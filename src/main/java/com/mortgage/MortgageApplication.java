420af7d8285bef29b3ffc313078a052fb6f3b84d
leonardo.moraes <leonardo.moraes@justeattakeaway.com>
2025-12-07 19:21:23 +0100

    First commit - Springboot application skeleton.

---
+++ src/main/java/com/mortgage/MortgageApplication.java
@@ -0,0 +1,15 @@
+package com.mortgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class MortgageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MortgageApplication.class, args);
+    }
+}
+
